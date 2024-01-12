47c38741e0f58301dbb7d1ac56ff8180f0ff7923
Vincent FAUGERE <faugere.vincent@hotmail.fr>
2024-01-12 17:24:45 +0100

    soft ok

--- software/app/main.c
+++ software/app/main.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
+#include "../bsp/HAL/inc/sys/alt_stdio.h"
+//#include “../bsp/system.h”
+//#include “altera_avalon_pio_regs.h”
 
 int main()	{
-	printf("Hello world");
-	
+	alt_printf("Hello world");
+
 	return 0;
 }
